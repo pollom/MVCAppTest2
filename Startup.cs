95844ca5240037dfb892a064bd7f7fc2c68ca589
Mike Pollock <pollom03@gmail.com>
2020-08-02 13:50:22 -0500

    Initial commit

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MVCAppTest.Startup))]
+namespace MVCAppTest
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
